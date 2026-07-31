ccc1466abb2fed5d2ee56568cfd0f4521497fb93
rajdip123 <rajdeepcoc115@gmail.com>
2026-07-31 21:34:55 +0530

    Remove download landing

--- src/LandingWrapper.tsx
+++ src/LandingWrapper.tsx
@@ -1,13 +1,5 @@
-import React, { useState } from 'react';
-import App from './App';
-import DownloadLanding from './DownloadLanding';
+import App from "./App";
 
 export default function LandingWrapper() {
-  const [showApp, setShowApp] = useState(false);
-  
-  if (showApp) {
-    return <App />;
-  }
-  
-  return <DownloadLanding onPreview={() => setShowApp(true)} />;
+  return <App />;
 }
